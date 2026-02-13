b6cf950f477e62bf8eb0ae70657d09f3376dad0e
TatsuyaYamaguchi <yamaguchi.tatsuya@itfllc.co.jp>
2026-02-13 16:33:33 +0900

    first commit

---
+++ src/main/java/com/springPET_STORE/SpringPetStoreApiApplication.java
@@ -0,0 +1,13 @@
+package com.springPET_STORE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPetStoreApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPetStoreApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springPET_STORE/SpringPetStoreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springPET_STORE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPetStoreApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
